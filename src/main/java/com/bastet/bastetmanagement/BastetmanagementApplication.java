d7245bcb10cc13e3a06103df644da94341999393
xxmehmetali <xxmehmetali@hotmail.com>
2022-11-14 22:22:06 +0300

    initial_commit

---
+++ src/main/java/com/bastet/bastetmanagement/BastetmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.bastet.bastetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BastetmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BastetmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bastet/bastetmanagement/BastetmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bastet.bastetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BastetmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
